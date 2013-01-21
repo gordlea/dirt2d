cbadbe2a025ea1d971330adcc6bc07d467837cca
Gord Lea <gord@10sheet.com>
2013-01-21 15:51:33 -0800

    Got projectiles going

---
+++ public/js/dirt2d2.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: gord
+ * Date: 2013-01-21
+ * Time: 1:15 PM
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ public/js/uuid.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: gord
+ * Date: 2013-01-21
+ * Time: 3:25 PM
+ * To change this template use File | Settings | File Templates.
+ */
